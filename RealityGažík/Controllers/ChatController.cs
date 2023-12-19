6cce9d2519535cae8364600fc72cf2fea9380ee3
Marcel Gažík <GazikMarcel@litv.sssvt.cz>
2023-12-19 08:45:11 +0100

    chat update

--- RealityGažík/Controllers/ChatController.cs
+++ RealityGažík/Controllers/ChatController.cs
@@ -9,9 +9,22 @@ namespace RealityGažík.Controllers
         public IActionResult Index()
         {
             List<Inquiry> inquiries = MyContext.Inquiries.ToList();
+            //List<Inquiry> blabla = MyContext.Inquiries.Where(x => { })
+            //inquiries = inquiries.Where(x =>
+            //{
+            //    if (this.isAdmin && this.id == x.idBroker)
+            //    {
+            //        return x.idBroker == this.id;
+            //    }
+            //    else if (this.isAdmin)
+            //    {
+            //        return true;
+            //    }
+
+            //    return x.idUser == this.id;
+            //}).ToList();
 
             this.ViewBag.Inquiries = inquiries;
-
             List<Message> lastMsg = new List<Message>();
             foreach (Inquiry item in inquiries)
             {
@@ -23,14 +36,23 @@ namespace RealityGažík.Controllers
         }
         public IActionResult Chat(int idInquiry)
         {
+            Inquiry inq = MyContext.Inquiries.Find(idInquiry)!;
+
             List<Message> messages = MyContext.Messages
             .Where(x => x.idInquiry == idInquiry)
             .ToList();
+
             this.ViewBag.Messages = messages;
             this.ViewBag.id = this.id;
             this.ViewBag.isAdmin = this.isAdmin;
 
-            //UpdateModel user = MyContext.Admins.Find(messages.idUser);
+            UpdateModel user = MyContext.Admins.Find(inq.idBroker)!;
+            if (user == null || user.id == this.id)
+            {
+               user = MyContext.Users.Find(inq.idUser)!;
+            }
+
+            this.ViewBag.name = user.name;
 
             return View();
         }
